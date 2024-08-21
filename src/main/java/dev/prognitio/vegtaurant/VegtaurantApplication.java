c754f5f2707570bf009822e7ee8d8e8688bedca3
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-08-21 16:12:37 -0500

    Initial commit

---
+++ src/main/java/dev/prognitio/vegtaurant/VegtaurantApplication.java
@@ -0,0 +1,13 @@
+package dev.prognitio.vegtaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegtaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegtaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/prognitio/vegtaurant/VegtaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prognitio.vegtaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegtaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
