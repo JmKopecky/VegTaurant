0e48ae508feb739f58c2a8ac27af1e0210271bae
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-09-12 20:56:52 -0500

    Created layout for the rest of the home screen. Created transitions. Created review area. Created interaction area. Began experimenting with animations.

--- src/main/resources/static/js/lenis_config.js
+++ src/main/resources/static/js/lenis_config.js
@@ -7,20 +7,47 @@ requestAnimationFrame(raf)
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
+    registerScroll();
+    registerAnimations();
+});
+
+let horizontalScroll;
+
+
+function registerScroll() {
     gsap.registerPlugin(ScrollTrigger)
 
     const siteContentScrollContainer = document.getElementById("site-content");
 
-    let totalScrollAmount = siteContentScrollContainer.scrollWidth;// - (0.525 * window.innerWidth);
-    gsap.to(siteContentScrollContainer, {
+    let totalScrollAmount = siteContentScrollContainer.scrollWidth - document.documentElement.clientWidth;// - (0.525 * window.innerWidth);
+    horizontalScroll = gsap.to(siteContentScrollContainer, {
         x: -totalScrollAmount,
         ease: "none",
         scrollTrigger: {
             trigger: "#site-content",
-            start: "top " + 0,
+            start: "top " + -1,
             end: "+=" + totalScrollAmount,
             pin: true,
-            scrub: true
+            scrub: true,
+            invalidateOnRefresh: true
         }
     });
-});
+}
+
+
+function registerAnimations() {
+    gsap.registerPlugin(ScrollTrigger)
+    //menu title fade and slide in.
+    const menuTitle = document.getElementById("menu-title");
+    //TODO
+    gsap.from(menuTitle, {
+        x: -300,
+        duration: 5,
+        //ease: "elastic",
+        scrollTrigger: {
+            trigger: menuTitle,
+            containerAnimation: horizontalScroll,
+            start: "center center",
+        }
+    });
+}
