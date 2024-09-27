67f119e32c5019c19fbdb1c30423c9ac4a67452c
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-09-26 20:19:08 -0500

    Created account database table, prepared setup for account info saving. PUSH for purposes of checking security issue.

--- src/main/java/dev/prognitio/vegtaurant/page_controllers/SignOnController.java
+++ src/main/java/dev/prognitio/vegtaurant/page_controllers/SignOnController.java
@@ -1,13 +1,63 @@
 package dev.prognitio.vegtaurant.page_controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.prognitio.vegtaurant.data_storage.Account;
+import dev.prognitio.vegtaurant.data_storage.AccountRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class SignOnController {
+    private final AccountRepository accountRepository;
+    public SignOnController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     @GetMapping("/signon")
     public String signOn(Model model) {
+        return "signon";
+    }
+
+
+    @PostMapping("/signon")
+    public String accountSignOn(Model model, @RequestBody String data) {
+        System.out.println(data);
+
+        Account account = new Account();
+        boolean shouldSave = true;
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode node = mapper.readTree(data);
+            account.setEmail(node.get("email").asText());
+            account.setPhone(node.get("phone").asText());
+            account.setPassword(node.get("password").asText()); //TODO hash passwords
+            account.setRewardPoints(0);
+            account.setFirstName(node.get("firstname").asText());
+            account.setLastName(node.get("lastname").asText());
+            account.setAddressLine1(node.get("addressline1").asText());
+            account.setAddressLine2(node.get("addressline2").asText());
+            account.setCity(node.get("city").asText());
+            account.setState(node.get("state").asText());
+            account.setZip(node.get("zip").asText());
+            account.setCountry(node.get("country").asText());
+            account.setCardNumber(node.get("cardnumber").asText());
+            account.setExpirationDate(node.get("cardexpirationdate").asText());
+            account.setSecurityCode(node.get("cardsecuritycode").asText());
+            account.setCardUserName(node.get("cardusername").asText());
+        } catch (JsonProcessingException e) {
+            shouldSave = false;
+            throw new RuntimeException(e);
+        }
+        if (shouldSave) {
+            accountRepository.save(account);
+        }
+
         return "signon";
     }
 }
--- src/main/resources/static/js/signon_animations.js
+++ src/main/resources/static/js/signon_animations.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 document.getElementById("signon-header").textContent = "Sign In";
                 document.getElementById("signon-text").textContent = "Sign in to your account to dramatically improve your shopping experience with us!";
                 document.getElementById("signon-button").textContent = "Sign In";
+                document.getElementById("signon-password-input").setAttribute("autocomplete", "current-password");
             }, 500);
 
             document.getElementById("header-content").classList.add("header-light");
@@ -34,6 +35,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 document.getElementById("signon-header").textContent = "Sign Up With Us!";
                 document.getElementById("signon-text").textContent = "Creating an account with us allows you to earn rewards, create meal plans, and streamline future orders!";
                 document.getElementById("signon-button").textContent = "Sign Up";
+                document.getElementById("signon-password-input").setAttribute("autocomplete", "new-password");
             }, 500);
 
             document.getElementById("header-content").classList.add("header-dark");
@@ -53,7 +55,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
             ease: "power1.inout",
             delay: 0.5
         })
-    })
+    });
+
+
+    document.getElementById("signon-button").addEventListener("click", () => {
+        fetch("/signon", {
+            method: "POST",
+            body: JSON.stringify({
+                "email": "em",
+                "password": "pw",
+                "firstname": "fn",
+                "lastname": "ln",
+                "addressline1": "adl1",
+                "addressline2": "adl2",
+                "city": "ci",
+                "state": "st",
+                "country": "ct",
+                "zip": "zip",
+                "phone": "phone",
+                "cardnumber": "card#",
+                "cardexpirationdate": "exp",
+                "cardsecuritycode": "seccode",
+                "cardusername": "cun"
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });//.then(r => window.location.replace(window.location.origin + "/account"));
+    });
 });
 
 function registerAnimations() {
