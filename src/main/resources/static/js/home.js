c4fc6d89104b46b5dad2b49343a673953eb8720c
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-11-26 17:39:06 -0600

    Substantial progress in home page design, pending approaches tile descriptions and the footer.

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -92,5 +92,72 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 function registerAnimations() {
+    for (const elem of document.getElementsByClassName("approaches-tile")) {
+        elem.addEventListener("mouseover", () => {
+            //show
+            toggleApproachesTile(elem, "show");
+        });
+        elem.addEventListener("mouseout", () => {
+            //hide
+            if (elem.getAttribute("data-approaches-tile-selected") !== "true") {
+                toggleApproachesTile(elem, "hide");
+            }
+        });
+        elem.addEventListener("click", () => {
+            if (elem.getAttribute("data-approaches-tile-selected") === "true") {
+                elem.setAttribute("data-approaches-tile-selected", "false");
+                toggleApproachesTile(elem, "hide");
+            } else {
+                elem.setAttribute("data-approaches-tile-selected", "true");
+                toggleApproachesTile(elem, "show");
+            }
+        });
+    }
+}
 
+function toggleApproachesTile(tile, mode) {
+    if (mode === "show") {
+        //swap content and header
+        //change background color
+        gsap.to(tile, {
+            backgroundColor: "rgba(0, 184, 55, 1)",
+            ease: "power1.out",
+            duration: 0.5
+        });
+        gsap.to(tile.getElementsByClassName("approaches-tile-heading")[0], {
+            opacity: 0,
+            ease: "linear",
+            duration: 0.25
+        });
+        setTimeout(() => {
+            tile.getElementsByClassName("approaches-tile-heading")[0].style.display = "none";
+            tile.getElementsByClassName("approaches-tile-content")[0].style.display = "block";
+            gsap.to(tile.getElementsByClassName("approaches-tile-content")[0], {
+                opacity: 1,
+                ease: "linear",
+                duration: 0.25
+            });
+        }, 250);
+    } else {
+        gsap.to(tile, {
+            backgroundColor: "rgba(0, 184, 55, 0)",
+            ease: "power1.out",
+            duration: 0.5
+        });
+        gsap.to(tile.getElementsByClassName("approaches-tile-content")[0], {
+            opacity: 0,
+            ease: "linear",
+            duration: 0.25
+        });
+        setTimeout(() => {
+            tile.getElementsByClassName("approaches-tile-content")[0].style.display = "none";
+            tile.getElementsByClassName("approaches-tile-heading")[0].style.display = "block";
+            gsap.to(tile.getElementsByClassName("approaches-tile-heading")[0], {
+                opacity: 1,
+                ease: "linear",
+                duration: 0.25
+            });
+        }, 250);
+
+    }
 }
